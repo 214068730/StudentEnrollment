b4fa76be5345bacf131c293c80dd61c3f35cc171
214068730 <siraajw19@gmail.com>
2017-02-15 15:37:57 +0200

    department service implemented

--- src/main/java/com/enrollment/service/Impl/DepartmentServiceImpl.java
+++ src/main/java/com/enrollment/service/Impl/DepartmentServiceImpl.java
@@ -1,5 +1,51 @@
 package com.enrollment.service.Impl;
 
-public class DepartmentServiceImpl {
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.enrollment.domain.Department;
+import com.enrollment.repository.DeparmentRepository;
+import com.enrollment.service.DepartmentService;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService{
+	
+	@Autowired
+	DeparmentRepository repo;
+	
+	
+
+	@Override
+	public Department create(Department entity) {
+		return repo.save(entity);
+	}
+
+	@Override
+	public Department readById(Long id) {
+		return repo.findOne(id);
+	}
+
+	@Override
+	public List<Department> readAll() {
+		List<Department> departmentList = new ArrayList<Department>();
+        Iterable<Department> departments = repo.findAll();
+        for (Department c : departments){
+        	departmentList.add(c);
+        }
+        return departmentList;
+	}
+
+	@Override
+	public Department update(Department entity) {
+		return repo.save(entity);
+	}
+
+	@Override
+	public void delete(Department entity) {
+		repo.delete(entity);
+	}
 
 }
