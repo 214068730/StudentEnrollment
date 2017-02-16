7cd21b80121c37383461146d047fbe1bf5e4d73e
214068730 <siraajw19@gmail.com>
2017-02-16 12:06:08 +0200

    AppSuite test class implemented

---
+++ src/test/java/com/enrollment/domain/AppSuite.java
@@ -0,0 +1,17 @@
+package com.enrollment.domain;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        TestAddress.class,
+        TestCourse.class,
+        TestDepartment.class,
+        TestLecturer.class,
+        TestStudent.class,
+        TestSubject.class
+})
+public class AppSuite {
+
+}
