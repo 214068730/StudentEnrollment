0b9b0d1edac4b5499a85704f001b7d610d50aff9
214068730 <siraajw19@gmail.com>
2017-03-03 15:05:50 +0200

    update tables

--- src/main/java/com/enrollment/domain/Student.java
+++ src/main/java/com/enrollment/domain/Student.java
@@ -40,10 +40,10 @@ public class Student implements Serializable {
 
 	public Student() {
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		DateFormat studentNumberGenerator = new SimpleDateFormat("yyyyMMdd");
+		DateFormat studentNumberGenerator = new SimpleDateFormat("yyyyHHddss");
 		Date date = new Date();
 		this.dateCreated = dateFormat.format(date).toString();
-		this.studentNumber =  studentNumberGenerator.format(date).toString();
+		this.studentNumber =  (studentNumberGenerator.format(date).toString()+System.currentTimeMillis()).substring(0, 8);
 
 	}
 
@@ -51,9 +51,9 @@ public class Student implements Serializable {
 			String studentSurname, Address studentAddress,String studentIdNumber,Roles role) {
 		super();
 		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
-		DateFormat studentNumberGenerator = new SimpleDateFormat("yyyyMMdd");
+		DateFormat studentNumberGenerator = new SimpleDateFormat("yyyyHHddss");
 		Date date = new Date();
-		this.studentNumber =  studentNumberGenerator.format(date).toString();
+		this.studentNumber =  (studentNumberGenerator.format(date).toString()+System.currentTimeMillis()).substring(0, 8);
 		this.studentName = studentName;
 		this.studentSurname = studentSurname;
 		this.studentAddress = studentAddress;
